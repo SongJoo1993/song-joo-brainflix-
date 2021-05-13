38ab732fdbe8eed15a9cb5c769892c6ef81fdb9c
SongJoo <songjoo29@gmail.com>
2021-05-13 01:34:40 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import './Header.scss';
+import userImage from "../assets/Images/Mohan-muruge.jpg";
+import logo from "../assets/Logo/Logo-brainflix.svg";
+
+function Header() {
+    return (
+        <header className="header">
+            <nav className="header__navigation">
+                <ul className="header__list">
+                    <img src={logo} alt="Logo" className="header__logo"></img>
+                    <input className="header__search-bar" placeholder="&#128269; Search"></input>
+                    <section className="header__video-upload">
+                        <button className="header__upload-box">+UPLOAD</button>
+                        <img src={userImage} alt="User" className="header__user-image"></img>
+                    </section>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
